804c9ef69f72c2bfa88486564a738a9e9aa3025c
mihaelahrihor <mihaelahrihor@icloud.com>
2025-02-17 23:52:52 -1000

    creare main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import controller.Aufgabe1Controller;
+import controller.Controller;
+import model.Charakter;
+import model.Ereignis;
+import model.Produkt;
+import parser.TSVParser;
+import view.View;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public static TSVParser tsvParser = TSVParser.getInstance();
+public static Aufgabe1Controller controller = new Aufgabe1Controller(tsvParser);
+
+public static void punktA() {
+    //a) lesen von der Datei und auf dem Bildschirm zeigen
+
+    System.out.println("\nPunkt a.\n");
+    String filePath = "marvel_konfrontationen.tsv";
+
+    try {
+        controller.parseResults(filePath);
+    } catch (IOException e) {
+        System.out.println("A apărut o eroare la citirea fișierului  " + e.getMessage());
     }
+
+    controller.printData();
+}
+
+
+public static void punktB() {
+    //b)
+    System.out.println("\nPunkt b.\n");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Gib eine Wert: ");
+    Double num = Double.parseDouble(scanner.nextLine());
+    List<String> result =  controller.filterHeldenNachEinfluss(num);
+
+    for (String str: result) {
+        System.out.println(str);
+    }
+}
+
+public static void punktC() {
+    //c)
+    System.out.println("\nPunkt c.\n");
+
+    List<String> result =  controller.filterGalaktischeKonfrontationen();
+
+    for (String str: result) {
+        System.out.println(str);
+    }
+
+}
+
+public static void punktD() {
+    //d)
+    System.out.println("\nPunkt d.\n");
+    String fileName = "ergebnis.txt";
+    controller.speichernBericht(fileName);
+}
+
+public static void aufgabe1() {
+    punktA();
+    punktB();
+    punktC();
+    punktD();
+}
+
+
 }
